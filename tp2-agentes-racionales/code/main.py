0ceecb2a5e195eaede05034a3c61b15aa38d7eeb
agussanchez97 <82063987+agussanchez97@users.noreply.github.com>
2021-08-23 19:20:31 -0300

    Update main.py

--- tp2-agentes-racionales/code/main.py
+++ tp2-agentes-racionales/code/main.py
@@ -2,8 +2,11 @@ import enviroment
 import agent
 import agentAleatorio
 
+#instancia de entorno
 aspirador=enviroment.Enviroment(32,32,0,0,10);
+#instancia de agente
 agent.Agent(aspirador)
 
+#instancia de agente aleatorio
 #agentAleatorio.Agent(aspirador)
 

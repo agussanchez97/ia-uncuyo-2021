3d192e0620294d7c5697927a1522d87c08240cdf
agussanchez97 <82063987+agussanchez97@users.noreply.github.com>
2021-08-30 21:06:57 -0300

    Update main.py

--- tp3-busquedas-no-informadas/code/main.py
+++ tp3-busquedas-no-informadas/code/main.py
@@ -1,1 +1,15 @@
+import agent
+import enviroment
+import node
 
+
+#m=input("Ingrese la posicion en X del agente ")
+#n=input("Ingrese la posicion en Y del agente")
+
+#p=input("Ingrese posición final X del agente")
+#q=input("Ingrese posicion final Y del agente")
+
+obstaculos = int(input("Ingrese cantidad de obstaculos"))
+entorno=enviroment.Enviroment(100,100,0,0,89,91,obstaculos)
+
+ag=agent.Agent(entorno)

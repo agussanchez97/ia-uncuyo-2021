d5560968e0a7ddf89128a0d07eccc3b5f18929c3
agussanchez97 <82063987+agussanchez97@users.noreply.github.com>
2021-10-06 22:42:53 -0300

    Update main.py

--- tp6-CSP/code/main.py
+++ tp6-CSP/code/main.py
@@ -1,1 +1,20 @@
 
+import backtraking
+
+from datetime import datetime
+import time
+
+#calcular el tiempo
+def getElapsedTime(startTime):
+    endTime = datetime.now()
+    elapsedTime = (endTime - startTime).microseconds / 1000
+    return elapsedTime
+    
+elapsedTime = 0
+startTime = datetime.now()
+n=input("Ingrese numero de reinas: ")
+ # Definir la capacidad máxima de la pila
+queen = [None] * int(n) #Almacenar la posición de fila de 8 reinas
+csp.decide_position(0,queen,0)
+elapsedTime = getElapsedTime(startTime)
+print("Success, Elapsed Time: %sms" % (str(elapsedTime)))

8f9da04d5385d2f0e25ebe5c651141da2a13a229
agussanchez97 <82063987+agussanchez97@users.noreply.github.com>
2023-06-27 14:50:47 -0300

    Implementación de Centroide mas Cercano

---
+++ proyecto_final/centroide.r
@@ -0,0 +1,53 @@
+install.packages("git2r")
+install.packages("caret")
+install.packages("ISLR")
+library(git2r)
+library(ISLR)
+library(caret)
+# Cargar el dataset Khan
+data(Khan)
+# Separar los conjuntos de entrenamiento y prueba
+xtrain <- Khan$xtrain
+ytrain <- Khan$ytrain
+xtest <- Khan$xtest
+ytest <- Khan$ytest
+
+# Obtener las etiquetas de clase
+clases <- unique(ytrain)
+
+
+# Inicializar una lista para almacenar los centroides
+centroides <- list()
+
+# Calcular los centroides para cada clase
+for (clase in clases) {
+  # Filtrar las instancias de la clase actual
+  instancias_clase <- xtrain[ytrain == clase, ]
+  
+  # Calcular el centroide promediando los valores de los atributos
+  centroide <- colMeans(instancias_clase)
+  print(centroide)
+  # Almacenar el centroide en la lista
+  centroides[[as.character(clase)]] <- centroide
+}
+print(centroides)
+
+distancias <- matrix(NA, nrow = nrow(xtest), ncol = length(clases))
+
+for (i in 1:nrow(xtest)) {
+  for (j in 1:length(clases)) {
+    # Calcular la distancia euclidiana entre la observación y el centroide
+    distancias[i, j] <- sqrt(sum((xtest[i, ] - centroides[[as.character(clases[j])]])^2))
+  }
+}
+
+clases_predichas <- clases[apply(distancias, 1, which.min)]
+
+# Convertir las variables a factores con los mismos niveles
+clases_predichas <- factor(clases_predichas, levels = unique(c(clases_predichas, ytest)))
+ytest <- factor(ytest, levels = unique(c(clases_predichas, ytest)))
+
+# Calcular la matriz de confusión
+confusionMatrix(clases_predichas, ytest)
+cm <- confusionMatrix(clases_predichas, ytest)
+print(cm)

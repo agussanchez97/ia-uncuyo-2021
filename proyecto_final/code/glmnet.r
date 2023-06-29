68ae4e44669c33d4aba9c28a2856638c9b4e14d2
LuciaCairo <luciicairo@gmail.com>
2023-06-28 21:41:56 -0300

    implementacion de glmnet con el dataset

---
+++ proyecto_final/code/glmnet.r
@@ -0,0 +1,50 @@
+# Instalar paquetes
+install.packages("git2r")
+install.packages("stringi")
+install.packages("caret")
+install.packages("ISLR")
+install.packages("glmnet")
+install.packages("MLmetrics")
+install.packages("pROC")
+
+# Cargar paquetes
+library(git2r)
+library(ISLR)
+library(caret)
+library(glmnet)
+library(MLmetrics)
+library(pROC)
+
+# Cargar el dataset Khan
+data(Khan)
+
+# Separar los conjuntos de entrenamiento y prueba
+xtrain <- Khan$xtrain
+ytrain <- Khan$ytrain
+xtest <- Khan$xtest
+ytest <- Khan$ytest
+
+# Entrenar el modelo GLMNET con validación cruzada
+cv_model <- cv.glmnet(xtrain, ytrain, family = "multinomial")
+
+# Obtener el valor óptimo de lambda
+lambda_min <- cv_model$lambda.min
+
+# Realizar predicciones en el conjunto de prueba
+predictions <- predict(glmnet_model, newx = xtest, s = lambda_min, type = "class")
+
+# Evaluar la precisión del modelo
+accuracy <- mean(predictions == ytest)
+print(accuracy)
+
+# Convertir las variables a factores con los mismos niveles
+predictions <- factor(predictions, levels = unique(c(predictions, ytest)))
+ytest <- factor(ytest, levels = unique(c(predictions, ytest)))
+
+# Calcular la matriz de confusión
+confusionMatrix(predictions, ytest)
+cm <- confusionMatrix(predictions, ytest)
+print(cm)
+
+
+

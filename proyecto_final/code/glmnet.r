fb47ff5f44255710d988a861fe283a22e5f5f1f1
LuciaCairo <luciicairo@gmail.com>
2023-07-12 15:16:49 -0300

    standardize = FALSE

---
+++ proyecto_final/code/glmnet.r
@@ -0,0 +1,45 @@
+# Instalar paquetes
+install.packages("git2r")
+install.packages("stringi")
+install.packages("caret")
+install.packages("ISLR")
+install.packages("glmnet")
+install.packages("MLmetrics")
+install.packages("pROC")
+
+# Cargar paquetes
+library(git2r)
+library(ISLR)
+library(caret)
+library(glmnet)
+library(MLmetrics)
+library(pROC)
+
+# Cargar el dataset Khan
+data(Khan)
+
+# Separar los conjuntos de entrenamiento y prueba
+xtrain <- Khan$xtrain
+ytrain <- Khan$ytrain
+xtest <- Khan$xtest
+ytest <- Khan$ytest
+
+# Entrenar el modelo GLMNET con validación cruzada
+model <- glmnet(xtrain, ytrain, family = "multinomial", standardize = FALSE)
+
+# Realizar predicciones en el conjunto de prueba
+predictions <- predict(model, newx = xtest, type = "class")
+
+
+# Evaluar la precisión del modelo
+accuracy <- mean(predictions == ytest)
+print(accuracy)
+
+# Convertir las variables a factores con los mismos niveles
+predictions <- factor(predictions, levels = unique(c(predictions, ytest)))
+ytest <- factor(ytest, levels = unique(c(predictions, ytest)))
+
+# Calcular la matriz de confusión
+confusionMatrix(predictions, ytest)
+cm <- confusionMatrix(predictions, ytest)
+print(cm)
